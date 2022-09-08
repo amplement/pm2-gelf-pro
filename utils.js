e09427bab25a52b6e57ce9e4d868a2b032106221
Morgan Denis <morgan.denis.60@gmail.com>
2022-09-08 16:24:52 +0200

    feat: improve global parsing and http support

---
+++ utils.js
@@ -0,0 +1,9 @@
+const os = require("os");
+
+function computeBaseExtras(log) {
+	return {
+		host: os.hostname(),
+		applicationName: log.process.name,
+	};
+}
+module.exports = { computeBaseExtras };
