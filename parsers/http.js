4544263ce83a67cb791d61ac5a73d420ead35b29
Morgan Denis <morgan.denis.60@gmail.com>
2022-09-09 17:56:39 +0200

    feat: split url into group

--- parsers/http.js
+++ parsers/http.js
@@ -1,3 +1,60 @@
+function getVersion(urlPart) {
+    if (!!urlPart.match(/v[0-9]{1,3}/g)) {
+        return parseInt(urlPart.replace('v', ''), 10);
+    }
+    return 1;
+}
+
+function isUuid(urlPart) {
+    return !!urlPart.match(/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/);
+}
+
+function extractQueryParams(url) {
+    const [baseUrl, queryParams] = url.split('?');
+    return { baseUrl, queryParams };
+}
+
+function extractRequestGroup(url) {
+    const { baseUrl, queryParams } = extractQueryParams(url);
+    const group = { queryParams, admin: false };
+    const splittedUrl = baseUrl.split('/');
+    group.version = getVersion(splittedUrl[0]);
+    if (group.version !== 1) {
+        splittedUrl.shift();
+    }
+    if (splittedUrl[0] === 'admin') {
+        group.admin = true;
+        splittedUrl.shift();
+    }
+    group.type = splittedUrl.shift();
+    if (splittedUrl[0] && isUuid(splittedUrl[0])) {
+        group._entity = splittedUrl.shift();
+    }
+
+    if (!splittedUrl[0]) {
+        return group;
+    }
+
+    if (!isUuid(splittedUrl[0])) {
+        group.subType = splittedUrl.shift();
+    }
+
+    if (!splittedUrl[0]) {
+        return group;
+    }
+
+    if (isUuid(splittedUrl[0])) {
+        group._subEntity = splittedUrl.shift();
+    }
+
+    if (!splittedUrl[0]) {
+        return group;
+    }
+    group.rest = splittedUrl.join('/');
+
+    return group;
+}
+
 function parser(log) {
     const [ip, , country, , httpVerb, url, responseCode, executionTime, userAgent, _client] = log
         .trim()
@@ -10,7 +67,8 @@ function parser(log) {
         responseCode,
         executionTime: parseFloat(executionTime.replace('rt=', '')),
         userAgent,
-        _client
+        _client,
+        ...extractRequestGroup(url)
     };
 }
 
