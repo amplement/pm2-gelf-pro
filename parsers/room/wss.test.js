b9b00829365d05923b71487ea704892a9ecd09b5
Morgan Denis <morgan.denis.60@gmail.com>
2022-10-04 16:36:52 +0200

    feat: extract token from WRTC logs

--- parsers/room/wss.test.js
+++ parsers/room/wss.test.js
@@ -90,7 +90,7 @@ describe('room-wss', () => {
 
         it('should parse correctly WRTC_SDP_ANSWER event log', () => {
             const fullLog =
-                '2022-09-19T09:12:30.871Z api:debug:wss:wrtc ⭡ WRTC_SDP_ANSWER to _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 ip 79.91.77.197 _entity 9038f83c-598a-4343-9f9d-84de1a0c048c';
+                '2022-09-19T09:12:30.871Z api:debug:wss:wrtc ⭡ WRTC_SDP_ANSWER to _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 ip 79.91.77.197 _entity 9038f83c-598a-4343-9f9d-84de1a0c048c token 99d8e78e-fed9-41ee-985e-19803c2c4b39';
             const { body: log, head } = prepareLog(fullLog);
             expect(parser(log, head)).toStrictEqual({
                 parser: 'room-wss',
@@ -100,6 +100,7 @@ describe('room-wss', () => {
                     _spark: '1c8be699-c2e5-4180-8750-8b18ea19ac40'
                 },
                 ip: '79.91.77.197',
+                token: '99d8e78e-fed9-41ee-985e-19803c2c4b39',
                 _entity: '9038f83c-598a-4343-9f9d-84de1a0c048c',
                 direction: 'emitting'
             });
@@ -107,7 +108,7 @@ describe('room-wss', () => {
 
         it('should parse correctly WRTC_SDP_OFFER event log', () => {
             const fullLog =
-                '2022-09-19T09:12:30.705Z api:debug:wss:wrtc ⭡ WRTC_SDP_OFFER to _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 ip 79.91.77.197 _entity b44db1b4-fbb9-43ee-84f8-16fb77a84f00';
+                '2022-09-19T09:12:30.705Z api:debug:wss:wrtc ⭡ WRTC_SDP_OFFER to _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 ip 79.91.77.197 _entity b44db1b4-fbb9-43ee-84f8-16fb77a84f00 token 99d8e78e-fed9-41ee-985e-19803c2c4b39';
             const { body: log, head } = prepareLog(fullLog);
             expect(parser(log, head)).toStrictEqual({
                 parser: 'room-wss',
@@ -117,6 +118,7 @@ describe('room-wss', () => {
                     _spark: '1c8be699-c2e5-4180-8750-8b18ea19ac40'
                 },
                 ip: '79.91.77.197',
+                token: '99d8e78e-fed9-41ee-985e-19803c2c4b39',
                 _entity: 'b44db1b4-fbb9-43ee-84f8-16fb77a84f00',
                 direction: 'emitting'
             });
@@ -124,7 +126,7 @@ describe('room-wss', () => {
 
         it('should parse correctly WRTC_PC_CLOSE event log', () => {
             const fullLog =
-                'api:debug:wss:wrtc ⭡ WRTC_PC_CLOSE to _client 8b5a72de-0adb-411f-9597-5f646221f646 _spark fa45db0b-65b0-4f17-91e6-01b978ea129c ip 78.119.123.160 _entity a0837aae-71cf-45e2-8e10-e0c48d818623';
+                'api:debug:wss:wrtc ⭡ WRTC_PC_CLOSE to _client 8b5a72de-0adb-411f-9597-5f646221f646 _spark fa45db0b-65b0-4f17-91e6-01b978ea129c ip 78.119.123.160 _entity a0837aae-71cf-45e2-8e10-e0c48d818623 token 99d8e78e-fed9-41ee-985e-19803c2c4b39';
             const { body: log, head } = prepareLog(fullLog);
             expect(parser(log, head)).toStrictEqual({
                 parser: 'room-wss',
@@ -134,6 +136,7 @@ describe('room-wss', () => {
                     _spark: 'fa45db0b-65b0-4f17-91e6-01b978ea129c'
                 },
                 ip: '78.119.123.160',
+                token: '99d8e78e-fed9-41ee-985e-19803c2c4b39',
                 code: 'WRTC_PC_CLOSE',
                 direction: 'emitting'
             });
@@ -143,7 +146,7 @@ describe('room-wss', () => {
     describe('receiving', () => {
         it('should parse correctly WRTC_ICE_CANDIDATES event log', () => {
             const fullLog =
-                '2022-09-19T09:12:31.288Z api:info:wss:wrtc ⭣ WRTC_ICE_CANDIDATES from _user 0e804ed5-ccb6-4f02-9426-c9efa268a315 _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa device web ip 79.91.77.197 _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 _entity b596298d-070c-40e3-af53-43fcd903a5f3';
+                '2022-09-19T09:12:31.288Z api:info:wss:wrtc ⭣ WRTC_ICE_CANDIDATES from _user 0e804ed5-ccb6-4f02-9426-c9efa268a315 _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa device web ip 79.91.77.197 _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 _entity b596298d-070c-40e3-af53-43fcd903a5f3 token 99d8e78e-fed9-41ee-985e-19803c2c4b39';
             const { body: log, head } = prepareLog(fullLog);
             expect(parser(log, head)).toStrictEqual({
                 parser: 'room-wss',
@@ -155,6 +158,7 @@ describe('room-wss', () => {
                 },
                 device: 'web',
                 ip: '79.91.77.197',
+                token: '99d8e78e-fed9-41ee-985e-19803c2c4b39',
                 code: 'WRTC_ICE_CANDIDATES',
                 direction: 'receiving'
             });
@@ -162,7 +166,7 @@ describe('room-wss', () => {
 
         it('should parse correctly WRTC_CONNECTION_STATE_CHANGE event log', () => {
             const fullLog =
-                '2022-09-19T09:12:31.220Z api:info:wss:wrtc ⭣ WRTC_CONNECTION_STATE_CHANGE from _user 0e804ed5-ccb6-4f02-9426-c9efa268a315 _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa device web ip 79.91.77.197 _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 _entity b596298d-070c-40e3-af53-43fcd903a5f3';
+                '2022-09-19T09:12:31.220Z api:info:wss:wrtc ⭣ WRTC_CONNECTION_STATE_CHANGE from _user 0e804ed5-ccb6-4f02-9426-c9efa268a315 _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa device web ip 79.91.77.197 _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 _entity b596298d-070c-40e3-af53-43fcd903a5f3 token 99d8e78e-fed9-41ee-985e-19803c2c4b39';
             const { body: log, head } = prepareLog(fullLog);
             expect(parser(log, head)).toStrictEqual({
                 parser: 'room-wss',
@@ -174,6 +178,7 @@ describe('room-wss', () => {
                 },
                 device: 'web',
                 ip: '79.91.77.197',
+                token: '99d8e78e-fed9-41ee-985e-19803c2c4b39',
                 code: 'WRTC_CONNECTION_STATE_CHANGE',
                 direction: 'receiving'
             });
@@ -181,7 +186,7 @@ describe('room-wss', () => {
 
         it('should parse correctly WRTC_ICE_CONNECTION_STATE_CHANGE event log', () => {
             const fullLog =
-                '2022-09-19T09:12:31.215Z api:info:wss:wrtc ⭣ WRTC_ICE_CONNECTION_STATE_CHANGE from _user 0e804ed5-ccb6-4f02-9426-c9efa268a315 _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa device web ip 79.91.77.197 _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 _entity b596298d-070c-40e3-af53-43fcd903a5f3';
+                '2022-09-19T09:12:31.215Z api:info:wss:wrtc ⭣ WRTC_ICE_CONNECTION_STATE_CHANGE from _user 0e804ed5-ccb6-4f02-9426-c9efa268a315 _client 7bbb59b5-37a4-48c7-bda1-09160ce781aa device web ip 79.91.77.197 _spark 1c8be699-c2e5-4180-8750-8b18ea19ac40 _entity b596298d-070c-40e3-af53-43fcd903a5f3 token 99d8e78e-fed9-41ee-985e-19803c2c4b39';
             const { body: log, head } = prepareLog(fullLog);
             expect(parser(log, head)).toStrictEqual({
                 parser: 'room-wss',
@@ -193,6 +198,7 @@ describe('room-wss', () => {
                 },
                 device: 'web',
                 ip: '79.91.77.197',
+                token: '99d8e78e-fed9-41ee-985e-19803c2c4b39',
                 code: 'WRTC_ICE_CONNECTION_STATE_CHANGE',
                 direction: 'receiving'
             });
