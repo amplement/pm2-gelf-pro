15355a779bfc3f9fb8f57fb04ec9dcd280bc1c56
cédric <cedbale@gmail.com>
2024-11-29 15:12:48 +0100

    feat: onboard retriedTime,device spec and country

--- parsers/room/pcs-stats.js
+++ parsers/room/pcs-stats.js
@@ -1,4 +1,12 @@
+const DeviceDetector = require('node-device-detector');
+const DeviceHelper = require('node-device-detector/helper');
+
 const { getUuidValue, getMatchSimpleValue } = require('../../utils');
+
+const detector = new DeviceDetector({
+    clientIndexes: true,
+    deviceIndexes: true
+});
 
 function isParseable(head) {
     return (
@@ -15,12 +23,23 @@ function parser(log, head) {
     const parsedData = {
         parser: 'room-pcs-stats'
     };
+
+    const userAgent = getMatchSimpleValue(log, /(userAgent "([^"\\]*(?:\\.[^"\\]*)*)")/);
+    if (userAgent !== '-') {
+        const result = detector.detect(log);
+        parsedData.uaIsBrowser = DeviceHelper.isBrowser(result);
+        parsedData.uaOsName = result.os.name || '-';
+        parsedData.uaClientName = result.client.name || '-';
+    }
+
     parsedData.token = getUuidValue(log, 'token');
     parsedData._entity = getUuidValue(log, '_entity');
     parsedData._user = getUuidValue(log, '_user');
     parsedData._client = getUuidValue(log, '_client');
+    parsedData.country = getMatchSimpleValue(log, /(country [A-Z-]+)/);
     parsedData.pcType = getMatchSimpleValue(log, /(pcType [a-zA-Z-]+)/);
     parsedData.pcState = getMatchSimpleValue(log, /(pcState [a-zA-Z-]+)/);
+    parsedData.retriedTimes = getMatchSimpleValue(log, /(retriedTimes [0-9]+)/);
     parsedData.iceState = getMatchSimpleValue(log, /(iceState [a-zA-Z-]+)/);
     parsedData.profileKey = getMatchSimpleValue(log, /(profileKey [a-zA-Z-]+)/);
 
