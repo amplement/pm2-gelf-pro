3ad98cb90021cc16a4e49a3eebc6e53a5f36b9fc
cédric <cedbale@gmail.com>
2024-12-09 16:06:24 +0100

    fix: set retriedTimes as number

--- parsers/room/pcs-stats.js
+++ parsers/room/pcs-stats.js
@@ -40,7 +40,7 @@ function parser(log, head) {
     parsedData.country = getMatchSimpleValue(log, /(country [A-Z-]+)/);
     parsedData.pcType = getMatchSimpleValue(log, /(pcType [a-zA-Z-]+)/);
     parsedData.pcState = getMatchSimpleValue(log, /(pcState [a-zA-Z-]+)/);
-    parsedData.retriedTimes = Number(getMatchSimpleValue(log, /(retriedTimes [0-9]+)/));
+    parsedData.retriedTimes_int = Number(getMatchSimpleValue(log, /(retriedTimes [0-9]+)/));
     parsedData.iceState = getMatchSimpleValue(log, /(iceState [a-zA-Z-]+)/);
     parsedData.profileKey = getMatchSimpleValue(log, /(profileKey [a-zA-Z-]+)/);
 
